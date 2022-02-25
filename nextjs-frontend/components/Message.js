cea0a28cde4dd96fcd602b23aecb6ba11e7f3b22
d3vv3 <jaimecondel@gmail.com>
2022-02-25 02:05:26 +0100

    enh: engage

--- nextjs-frontend/components/Message.js
+++ nextjs-frontend/components/Message.js
@@ -8,13 +8,13 @@ function Message(props) {
     <div className="message-container">
       <div className="message">
         <p>
-          ¡El primer pedido está de camino! Recibirás un correo cuando haya llegado.
+          ¡El primer pedido ya está aquí! Mira tu correo para las instrucciones de recogida.
         </p>
         <p>
           En <b>marzo</b> haremos el <b>segundo y último</b> pedido del curso. ¡Aprovecha y pide la tuya!
         </p>
         <p>
-          Para cualquier consulta, hablar a Pedro: 722 51 56 06
+          ¿Alguna consulta? Escríbenos a 722 51 56 06 (Pedro).
         </p>
       </div>
     </div>
