8ff2ad3401073a69b227e89225a19044105dec1b
d3vv3 <jaimecondel@gmail.com>
2022-03-28 00:37:54 +0200

    upd: message

--- nextjs-frontend/components/Message.js
+++ nextjs-frontend/components/Message.js
@@ -8,10 +8,7 @@ function Message(props) {
     <div className="message-container">
       <div className="message">
         <p>
-          ¡El primer pedido ya está aquí! Mira tu correo para las instrucciones de recogida.
-        </p>
-        <p>
-          En <b>marzo</b> haremos el <b>segundo y último</b> pedido del curso. ¡Aprovecha y pide la tuya!
+          El <b>31 de marzo</b> haremos el <b>segundo y último</b> pedido del curso. ¡Aprovecha y pide la tuya!
         </p>
         <p>
           ¿Alguna consulta? Escríbenos a 722 51 56 06 (Pedro).
